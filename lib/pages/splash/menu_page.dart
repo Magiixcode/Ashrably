b3d90598ef9af8a15e636e9b85e23767f77706ab
Abdulkhaleq Dagher <abdodagher24@gmail.com>
2024-08-15 15:36:28 +0300

    create menue page

--- lib/pages/splash/menu_page.dart
+++ lib/pages/splash/menu_page.dart
@@ -1,20 +1,108 @@
 import 'package:ashrably_app/constens.dart';
+import 'package:ashrably_app/pages/setting_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_zoom_drawer/flutter_zoom_drawer.dart';
 
-class MenuPage extends StatefulWidget {
+class MenuPage extends StatelessWidget {
   const MenuPage({super.key});
 
-  @override
-  State<MenuPage> createState() => _MenuPageState();
-}
-
-class _MenuPageState extends State<MenuPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: KPrimaryColor,
-      body: Column(
-        children: [],
+      body: Padding(
+        padding: const EdgeInsets.only(top: 80),
+        child: Column(
+          children: [
+            ListTile(
+              leading: const Icon(
+                Icons.home,
+                color: Colors.white,
+              ),
+              title: const Text(
+                'الصفحة الرئيسية ',
+                style: TextStyle(
+                  fontFamily: 'Marhey',
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              onTap: () {
+                ZoomDrawer.of(context)!.toggle();
+              },
+            ),
+            ListTile(
+              leading: const Icon(
+                Icons.settings,
+                color: Colors.white,
+              ),
+              title: const Text(
+                'الاعدادات',
+                style: TextStyle(
+                  fontFamily: 'Marhey',
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (BuildContext context) {
+                  return SettingPage();
+                }));
+              },
+            ),
+            ListTile(
+              leading: const Icon(
+                Icons.call,
+                color: Colors.white,
+              ),
+              title: const Text(
+                'تواصل معانا',
+                style: TextStyle(
+                  fontFamily: 'Marhey',
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: const Icon(
+                Icons.group,
+                color: Colors.white,
+              ),
+              title: const Text(
+                'عنا',
+                style: TextStyle(
+                  fontFamily: 'Marhey',
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: const Icon(
+                Icons.share,
+                color: Colors.white,
+              ),
+              title: const Text(
+                'مشاركة التطبيق',
+                style: TextStyle(
+                  fontFamily: 'Marhey',
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              onTap: () {},
+            )
+          ],
+        ),
       ),
     );
   }
