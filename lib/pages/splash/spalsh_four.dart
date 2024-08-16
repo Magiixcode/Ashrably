47296d2bb06ccbec57af11d27dec9dcb858f55b1
Abdulkhaleq Dagher <abdodagher24@gmail.com>
2024-08-16 06:17:54 +0300

    add drobdownbutton

--- lib/pages/splash/spalsh_four.dart
+++ lib/pages/splash/spalsh_four.dart
@@ -5,9 +5,23 @@ import 'package:ashrably_app/widget/custom_text-filed.dart';
 import 'package:ashrably_app/widget/next_button.dart';
 import 'package:flutter/material.dart';
 
-class Spalsh4 extends StatelessWidget {
+class Spalsh4 extends StatefulWidget {
   const Spalsh4({super.key});
 
+  @override
+  State<Spalsh4> createState() => _Spalsh4State();
+}
+
+class _Spalsh4State extends State<Spalsh4> {
+  String _dropdownValue = '1';
+  var items = [
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -66,7 +80,41 @@ class Spalsh4 extends StatelessWidget {
               text: "عاوز تشرب كام لتر في اليوم ؟",
             ),
           ),
-          const Positioned(bottom: 370, left: 160, child: CustomTextFiled()),
+          Positioned(
+            bottom: 370,
+            left: 160,
+            child: Container(
+              width: 70,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: Colors.white,
+              ),
+              child: Center(
+                child: DropdownButton(
+                  items: items.map((String item) {
+                    return DropdownMenuItem(
+                      value: item,
+                      child: Text(item),
+                    );
+                  }).toList(),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      _dropdownValue = newValue!;
+                    });
+                  },
+                  value: _dropdownValue,
+                  borderRadius: BorderRadius.circular(10),
+                  icon: const Icon(Icons.keyboard_arrow_down),
+                  iconSize: 20,
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.black,
+                  ),
+                  underline: Container(),
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
